03d0e44dc7bcb24a5456a2462e22bdcf4c6ce24b
Tisdale511 <tisdalefry@gmail.com>
2020-05-12 12:21:46 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    name = "Tisdale Fry"
+    hometown = "Yukon, OK"
+    favorite_song = "Shake Your Body Down" 
+
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is #{name}"
+    end
+
+    get '/hometown' do
+        "My hometown is #{hometown}"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is #{favorite_song}"
+    end
+
 end
